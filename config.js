afcfae26944bd578cbf1385b6f6733fa34310494
Frognana <106753221+fanxxxvn@users.noreply.github.com>
2022-06-02 21:02:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const CONFIG = {
     titleWeb: " Yêu em ",
     introTitle: 'Đinh Tử Hương',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    desc: 'Làm người yêu anh nha 🥰',
+    btnYes: 'Oke luôn <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình em <3',
     btnReply: 'Gửi cho em <3',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Cuối tuần sau, đi uống tà tưa với a .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com//Confess-Yeuem' //https://www.facebook.com/frog.apk/.
+    messLink: 'https://www.facebook.com/frog.apk/'
 }
