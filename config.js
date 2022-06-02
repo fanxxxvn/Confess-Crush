9ca5b898ee65d5aab4ce446c1bb340947c1079fd
Frognana <106753221+fanxxxvn@users.noreply.github.com>
2022-06-02 21:07:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: " Yêu em ",
-    introTitle: 'Đinh Tử Hương',
+    introTitle: 'người tôi yêu Đinh Tử Hương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    desc: 'Làm người yêu anh nhé ',
+    btnYes: 'oke luôn <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình em <3',
-    btnReply: 'Gửi cho em <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu mình  <3',
+    btnReply: 'Gửi cho  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Cuối tuần sau, đi uống tà tưa với a .',
+    messDesc: 'Cuối tuần sau, đi uống tà tưa với a nha baby .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com//Confess-Yeuem' //https://www.facebook.com/frog.apk/.
+    messLink: '//https://www.facebook.com/frog.apk/'
 }
